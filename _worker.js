d8ce5160d7c8d91663cae34f7d5ba08239312916
berry-mannes <148453731+berry-mannes@users.noreply.github.com>
2024-04-22 13:53:11 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -101,7 +101,7 @@ export default {
                     default:
                         // return new Response('Not found', { status: 404 });
                         // For any other path, reverse proxy to 'www.baidu.com' and return the original response
-                        url.hostname = 'www.baidu.com';
+                        url.hostname = 'www.qq.com';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
@@ -746,8 +746,8 @@ async function handleUDPOutBound(webSocket, vlessResponseHeader, log) {
  * @returns {string}
  */
 function getVLESSConfig(userID, hostName) {
-    const vlessLink = `vless://${userID}\u0040www.gov.se:80?encryption=none&security=none&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2560#Misaka-workers`
-    const vlessTlsLink = `vless://${userID}\u0040www.gov.se:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2560#Misaka-workers-TLS`
+    const vlessLink = `vless://${userID}\u0040${hostName}:80?encryption=none&security=none&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2560#${hostName}`
+    const vlessTlsLink = `vless://${userID}\u0040${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2560#${hostName}-TLS`
     return `
 下面是非 TLS 端口的节点信息及分享链接，可使用 CF 支持的非 TLS 端口：
 
@@ -773,9 +773,9 @@ SNI 域名：${hostName}
 
 ${vlessTlsLink}
 
+---------------------------------------------------------------
 提示：部分地区有 CF 默认域名被污染的情况，除非打开客户端的 TLS 分片功能，否则无法使用 TLS 端口的节点
 如为 Pages 部署的节点则只能使用 TLS 端口的节点
 ---------------------------------------------------------------
-更多教程，请关注：小御坂的破站
 `;
 }
